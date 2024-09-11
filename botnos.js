6269dc5f40f680a42bdbcf104276f201a30339e4
Lopecito23 <ericklsteven26@gmail.com>
2024-09-11 16:33:51 -0500

    Add files via upload

---
+++ botnos.js
@@ -0,0 +1,16 @@
+// //Esperar a que el contenido del DOM este completamente cargado
+// document.addEventListener('DOMContentLoaded', () => {
+// });
+// //console.log("Probando esta monda")
+// const boton = document.getElementById('Botoncito');
+// boton.addEventListener("click",() =>{
+// console.log("Diste click en el boton perra");
+// //alert("El mejor tirador de todos los tiempos en la NBA");
+// boton.textContent = ' ¡¿Enserio no sabes?! ';
+// boton.textContent = ' Nosotros somos.... ';
+// boton.addEventListener("click",() =>{
+//     boton.classList.add('test', 'blueviolet', 'd-none')
+//     document.getElementById('texto').classList.remove('d-none');
+// })
+
+// })
